58a7547efc62014930c1f598444563efd41996ad
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-12 21:20:19 +0100

    Initial commit

---
+++ app/src/main/java/app/a2ms/favrepos/MainActivity.java
@@ -0,0 +1,18 @@
+package app.a2ms.favrepos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.screen_container, new ListFragment())
+                    .commit();
+        }
+    }
+}
