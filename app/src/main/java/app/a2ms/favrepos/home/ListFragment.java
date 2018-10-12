5f0fd7feb66b1b1ae3e3520d5e947289188528d2
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-12 21:46:10 +0100

    Add viewModel

--- app/src/main/java/app/a2ms/favrepos/home/ListFragment.java
+++ app/src/main/java/app/a2ms/favrepos/home/ListFragment.java
@@ -1,8 +1,11 @@
 package app.a2ms.favrepos.home;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,10 +23,10 @@ public class ListFragment extends Fragment {
     RecyclerView listView;
     @BindView(R.id.tv_error)
     TextView errorTextView;
-    @BindView(R.id.loading_view)
-    View loadingView;
+    @BindView(R.id.loading_view) View loadingView;
 
     private Unbinder unbinder;
+    private ListViewModel viewModel;
 
     @Nullable
     @Override
@@ -31,6 +34,43 @@ public class ListFragment extends Fragment {
         View view = inflater.inflate(R.layout.screen_list, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+
+        listView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        listView.setAdapter(new RepoListAdapter(viewModel, this));
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+        viewModel.getRepos().observe(this, repos -> {
+            if (repos != null) {
+                listView.setVisibility(View.VISIBLE);
+            }
+        });
+        viewModel.getError().observe(this, isError -> {
+            //noinspection ConstantConditions
+            if (isError) {
+                errorTextView.setVisibility(View.VISIBLE);
+                listView.setVisibility(View.GONE);
+                errorTextView.setText(R.string.api_error_repos);
+            } else {
+                errorTextView.setVisibility(View.GONE);
+                errorTextView.setText(null);
+            }
+        });
+        viewModel.getLoading().observe(this, isLoading -> {
+            //noinspection ConstantConditions
+            loadingView.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            if (isLoading) {
+                errorTextView.setVisibility(View.GONE);
+                listView.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
@@ -42,4 +82,3 @@ public class ListFragment extends Fragment {
         }
     }
 }
-
